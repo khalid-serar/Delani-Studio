007391aceb6dc3eb444211102787a56c478ddcb6
khalid-serar <khalid.serar@student.moringaschool.com>
2021-09-13 19:10:38 +0300

    add bootstrap to css

---
+++ images/assets/js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1000');
+    });
+  });
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
+
+  
+//  
+  $(document).ready(function(){
+    $("form#formsubmit").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + "Hey we have received your message. we will get back to you soon.:)");
+      }
+      else {
+        alert(" enter a valid email and name!");
+      }
+      
+    });
+  
+  });
